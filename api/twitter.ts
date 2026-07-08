74799dfb6f1230a1b64f9cf9c444210478b3cffb
Peu Borges <148594362+MCookinho@users.noreply.github.com>
2026-07-08 01:51:29 -0300

    debug: show timeline structure

--- api/twitter.ts
+++ api/twitter.ts
@@ -321,7 +321,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         const tweets = extractTweets(result);
         const nextCursor = extractCursor(result);
 
-        return res.json({ tweets, nextCursor });
+        return res.json({
+          tweets, nextCursor,
+          _timeline: result?.data?.user?.result?.timeline?.timeline ?? null,
+        });
       }
 
       default:
